b19a75d530eb087728024a1c88620b583efef79d
Will Jacobson <willjacobson1@gmail.com>
2020-05-08 21:31:27 -0700

    module 1 problems complete

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,36 @@
 package module01
 
+// GCD : using the Euclidean algorithm
 func GCD(a, b int) int {
-	return 0
+	// Step 1: if b == 0, return a
+	for b != 0 {
+		// Step 2: a = b, b = a%b
+		a, b = b, a%b
+	}
+
+	return a
 }
+
+// GCDRecursive : using the Euclidean algorithm (recursively)
+func GCDRecursive(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return GCDRecursive(b, a%b)
+}
+
+/*
+a = 252
+b = 105
+
+a = 105
+b = 252%105 = 42
+
+a = 42
+b = 105%42 = 21
+
+a = 21
+b = 42%21 = 0
+
+=> 21
+*/
--- module01/std_demo/main.go
+++ module01/std_demo/main.go
@@ -5,9 +5,25 @@ import (
 	"fmt"
 )
 
+/*
+How to use:
+
+Can run `go run std_demo/main.go` and enter an int,
+then enter pairs of numbers manually
+
+Can run `go run std_demo/main.go < std_demo/input.txt`
+to feed in pairs of numbers from the input file
+
+Can run `go run std_demo/main.go < std_demo/input.txt > std_demo/output.txt`
+to feed in pairs of numbers from the input file and output them to a file
+*/
+
 func main() {
+	// This int controls how many times the loop runs
+	// (how many pairs of numbers this program is expecting)
 	var n int
 	fmt.Scanf("%d", &n)
+
 	for i := 0; i < n; i++ {
 		var a, b int
 		fmt.Scanf("%d %d", &a, &b)

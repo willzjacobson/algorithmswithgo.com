7db262646624ff69e10184153fc317cea2b225e7
Will Jacobson <willjacobson1@gmail.com>
2020-06-15 12:13:37 -0700

    fix insertion bug in b-tree

--- data_structures/b-tree.go
+++ data_structures/b-tree.go
@@ -4,33 +4,56 @@ import "fmt"
 
 // BTree : type representing a B-tree
 type BTree struct {
-	root BTreeNode
-	t    int
+	root     *BTreeNode
+	t        int
+	nullNode *BTreeNode
 }
 
 // BTreeNode : type representing a node of a B-tree
 type BTreeNode struct {
 	keys     []int
-	children []BTreeNode
+	children []*BTreeNode
 	n        int
 	leaf     bool
 }
 
 // AllocateNode : in a real implementation, this function would allocate a disk page to be used as a new node
-func AllocateNode() BTreeNode {
-	return BTreeNode{}
+func (t *BTree) AllocateNode() *BTreeNode {
+	x := BTreeNode{}
+	for i := 0; i < 2*t.t; i++ {
+		x.children = append(x.children, t.nullNode)
+	}
+	for i := 0; i < 2*t.t-1; i++ {
+		x.keys = append(x.keys, -1)
+	}
+	return &x
 }
 
 // BTreeCreate : creates B tree and adds an empty root
 // Runs in O(1) time
-func BTreeCreate(t int) BTree {
-	x := AllocateNode()
-	x.leaf = true
-	// Here is where we'd write the new node to disk
-	return BTree{
-		root: x,
-		t:    t,
+func BTreeCreate(t int) *BTree {
+	// create null node to use as place filler
+	nullNode := &BTreeNode{}
+	for i := 0; i < 2*t; i++ {
+		nullNode.children = append(nullNode.children, nullNode)
 	}
+
+	// create the tree
+	tree := BTree{
+		t:        t,
+		nullNode: nullNode,
+	}
+
+	// create root node
+	x := tree.AllocateNode()
+	x.leaf = true
+	tree.root = x
+
+	// create null node used to auto-populate children of newly allocated nodes
+	tree.nullNode = tree.AllocateNode()
+
+	// *Here is where we'd write the new node to disk
+	return &tree
 }
 
 // BTreeSplitChild : Split a full node into 2 nodes
@@ -39,22 +62,27 @@ func BTreeCreate(t int) BTree {
 // The keys in y that are > S move into a new node z,
 // which becomes a new child of x
 // Runs in O(t) time, since the loops go from 0 -> t-1
-func (t BTree) BTreeSplitChild(x BTreeNode, i int) {
-	z := AllocateNode()
-	y := x.children[i] // node to split
+func (t *BTree) BTreeSplitChild(x *BTreeNode, i int) {
+	z := t.AllocateNode()
+	y := x.children[i] // y is node to split
 	z.leaf = y.leaf    // if existing node to split is a leaf, so will be its new sibling
 	z.n = t.t - 1      // since y was full (and this has 2t-1 keys) and we are splitting y on S=y.keys[i], z will have t-1 keys
+
 	// move y's keys that are > S into node z (S = y.keys[t-1], due to Go's zero indexing. So this loop goes up to j=t-2)
 	for j := 0; j < t.t-1; j++ {
 		z.keys[j] = y.keys[j+t.t]
+		y.keys[j+t.t] = -1
 	}
 	// if y is not a leaf node, move its children over to node z
 	if !y.leaf {
 		for j := 0; j < t.t-1; j++ {
 			z.children[j] = y.children[j+t.t]
+			y.children[j+t.t] = t.nullNode
 		}
 	}
-	y.n = t.t - 1 // having moved t keys out of y, update its key count accordingly
+
+	y.n = t.t // having moved t-1 keys out of y, update its key count accordingly
+
 	// scooch the higher half of x's children up by 1 to make room for the new node Z
 	for j := x.n; j >= i+1; j-- {
 		x.children[j+1] = x.children[j]
@@ -65,8 +93,10 @@ func (t BTree) BTreeSplitChild(x BTreeNode, i int) {
 		x.keys[j+1] = x.keys[j]
 	}
 	x.keys[i] = y.keys[t.t-1] // Insert S into x's keys
-	x.n = x.n + 1             // adjust x's key count to account for adding S
-	// Here is where we'd perform a disck write operation for creating node z, and updating nodes x and y
+	y.keys[t.t-1] = -1        // nullify y.keys[t]
+	y.n--                     // lower y.n to account for nullifying y.keys[t]
+	x.n++                     // adjust x's key count to account for adding S
+	// *Here is where we'd perform a disk write operation for creating node z, and updating nodes x and y
 }
 
 // BTreeInsert : Insert node into an existing Btree
@@ -74,40 +104,46 @@ func (t BTree) BTreeSplitChild(x BTreeNode, i int) {
 // Runs in O(t logtn) time, since:
 // - calls to BTreeSplitChild run in O(t) time
 // - there will be at most logtn calls to BTreeInsertNonFull (height of BTree)
-func (t BTree) BTreeInsert(k int) {
+func (t *BTree) BTreeInsert(k int) {
 	r := t.root
 	if r.n == 2*t.t-1 {
 		// root node is full; split it into 2 nodes (thus increasing height of BTree by 1)
-		s := AllocateNode()
-		t.root = s
+		s := t.AllocateNode()
+		s.children[0] = r
 		s.leaf = false
-		s.children = append(s.children, r)
-		t.BTreeSplitChild(r, 0)
+		t.root = s
+		t.BTreeSplitChild(s, 0)
+		// call helper to manage the insertion of the new key. At this point, we know node r is not full.
+		t.BTreeInsertNonFull(s, k)
+	} else {
+		t.BTreeInsertNonFull(r, k)
 	}
-	// call helper to manage the insertion of the new key. At this point, we know node r is not full.
-	t.BTreeInsertNonFull(r, k)
 }
 
 // BTreeInsertNonFull : recursive function to manage insertion of a new key into the BTree
-func (t BTree) BTreeInsertNonFull(x BTreeNode, k int) {
-	i := x.n - 1
+func (t *BTree) BTreeInsertNonFull(x *BTreeNode, k int) {
+	i := x.n
 	if x.leaf {
-		for i >= 0 && k < x.keys[i] {
-			x.keys[i+1] = x.keys[i]
+		for x.n > 0 && i >= 1 && (x.keys[i-1] == -1 || k < x.keys[i-1]) {
 			i--
 		}
-		x.keys[i+1] = k
-		x.n++
+
+		// scooch the keys of x that are greater than k up 1 index to make room for k
+		for j := 2*t.t - 3; j >= i; j-- {
+			x.keys[j+1] = x.keys[j]
+		}
+		x.keys[i] = k // assign new key k to proper index of x.keys
+		x.n++         // having added the key to the node, increment the key count
 		// write updates to node x to disk
 	} else {
 		// find index of child node that serves as root of the subtree we need to dig into
-		for i >= 0 && k < x.keys[i] {
+		for i >= 0 && (x.keys[i] == -1 || k < x.keys[i]) {
 			i--
 		}
 		i++
-		// read x.children[i] from disk
+		// *Here is where we'd read x.children[i] from disk
+		// child node of x is full; split it
 		if x.children[i].n == 2*t.t-1 {
-			// child node of x is full; must be split
 			t.BTreeSplitChild(x, i)
 			// if key should go in the new node, increment the key index
 			if k > x.keys[i] {
@@ -124,7 +160,7 @@ func (t BTree) BTreeInsertNonFull(x BTreeNode, k int) {
 // time complexity is O(t logtn), since:
 //  - the method could run at most logt times
 //  - since x.n <= 2t-1, the for loop takes O(t) time
-func (t *BTree) BTreeSearch(x BTreeNode, k int) (BTreeNode, int) {
+func (t *BTree) BTreeSearch(x *BTreeNode, k int) (*BTreeNode, int) {
 	i := 0
 	// look for the index of the lowest k in x.keys that is greater than k
 	for i < x.n && k < x.keys[i] {
@@ -145,8 +181,27 @@ func (t *BTree) BTreeSearch(x BTreeNode, k int) (BTreeNode, int) {
 
 func main() {
 	t := BTreeCreate(2)
-	fmt.Println("B-tree:", t)
+	fmt.Println("Empty B-tree:", t.root.keys, t.root.children)
 	fmt.Println(t.root)
-	n, i := t.BTreeSearch(t.root, 0)
-	fmt.Println("n:", n, "i:", i)
+
+	n1, i1 := t.BTreeSearch(t.root, 13)
+	fmt.Println("Key not found:", "n:", n1, "i:", i1)
+
+	t.BTreeInsert(13)
+
+	n2, i2 := t.BTreeSearch(t.root, 13)
+	fmt.Println("Key found:", "n:", n2, "i:", i2)
+
+	t.BTreeInsert(7)
+	fmt.Println("Non Empty B-tree:", t.root, t.root.children)
+	t.BTreeInsert(24)
+	fmt.Println("Non Empty B-tree:", t.root, t.root.children)
+	fmt.Println("-=-=-=-=- post split:")
+	t.BTreeInsert(16)
+	fmt.Println("Non Empty B-tree:", t.root.children[0].keys, t.root.keys, t.root.children[1].keys)
+	t.BTreeInsert(5)
+	fmt.Println("Non Empty B-tree:", t.root.children[0].keys, t.root.keys, t.root.children[1].keys)
+	t.BTreeInsert(50)
+	t.BTreeInsert(51)
+	fmt.Println("Non Empty B-tree:", t.root.children, t.root.children[0].keys, t.root.keys, t.root.children[1].keys, t.root.children[2].keys)
 }
